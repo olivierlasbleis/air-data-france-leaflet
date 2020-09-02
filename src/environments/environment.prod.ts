e1cb654f46d783137bc08b4d8b44cdc5f0e0c132
olivierlasbleis <lasbleis.olivier29@gmail.com>
2020-09-02 16:29:18 +0200

    deploiement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // ajout d'une URL backend en mode production
-  backendUrl: 'https://air-data-france-api.herokuapp.com'
+  backendUrl: 'https://air-data-france-api.herokuapp.com',
+  backGeojsonUrl : 'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master'
 };
